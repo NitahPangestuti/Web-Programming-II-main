80ffd5cdb4207db2fc3a5a95f436029ebcb3e2e5
NitahPangestuti <56468088+NitahPangestuti@users.noreply.github.com>
2020-10-29 22:58:56 +0700

    tugas

---
+++ Project/pustakabooking/application/controllers/Perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class Perkenalan extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>PERKENALAN</h1>";
+		echo "<h2>Nama Saya Nitah Pangestuti</h2>";
+		echo "<h2>Saya tinggal di Pangkah</h2>";
+		echo "<h2>Saya suka menyanyi</h2>";
+	}
+}
